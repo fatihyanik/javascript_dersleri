4b6eccccd83753c451043ec1aa72bab86a39c43e
MuratFatih_FEdev_ _ <you@example.com>
2023-11-07 20:48:20 +0100

    added ödev.js

---
+++ 1/odev.js
@@ -0,0 +1,43 @@
+// 1
+const numbers = [1, 2, 3, 4, 5];
+// The below line should console.log: [5, 6, 7, 8, 10]
+console.log(numbersAddFive); 
+
+// 2 
+const sayilar = [10, 11, 12, 13, 14];
+// The below line should console.log: ["10: ", "11: ", "12: ", "13: ", "14: "] 
+console.log(numbersReformatted);
+
+// 3
+const words = ["kalemler", "elmalar", "arabalar", "insanlar"];
+// The below line should console.log: ["kalem", "elma", "araba", "insan"]
+console.log(singularWords);
+
+// 4: ücüncü sorudaki array'in ilk harfleri yazdirin
+// The below line should console.log: ["k", "e", "a", "i"]
+console.log(firstLetters);
+
+// 5:
+// ucuncu sorudaki array'in ilk harfini büyük geri kalanini kücük yazdirin
+// The below line should console.log: ["Kalemler", "Elmalar", "Arabalar", "Insanlar"]
+console.log(capitalizedWords);
+
+const hayvanlar = ["kedi", "köpek", "kaplumbaga", "kaplan"]
+
+// 6:
+// hayvan isminin harf sayisi 5 ve 5 ten kücük olani yazdirin
+// The below line should console.log: ["kedi", "köpek"]
+console.log(petsShortNames);
+
+// 7 - hayvan isimleri icinde p olanlari yazdirin
+// The below line should console.log: ["köpek", "kaplumbaga", "kaplan"] 
+console.log(petsPNames);
+
+const numaralar = [3, 7, 4, 5, 10];
+// 8 - numaralar icinde 4 ten büyük olanlari yazdirin 
+// The below line should console.log: [7, 5, 10]
+console.log(numbersBiggerThanFour);
+
+// 9 - numaralar arrayindeki cift sayilari yazdir
+// The below line should console.log: [4, 10]
+console.log(evenNumbers);
