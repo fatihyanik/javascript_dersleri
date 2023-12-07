811417516ccfeffa30f28772d6bb29b0733ebdf9
MuratFatih_FEdev_ _ <you@example.com>
2023-12-07 09:16:15 +0100

    added interview questions

---
+++ 7-whileLoop/1.js
@@ -0,0 +1,30 @@
+let a = 40;
+let b = 20;
+while (a >= b) {
+    a -= 5;
+    b += 10
+    console.log(a, b);
+}
+
+//5! 5*4*3*2*1
+// for loop ile
+let multiply = 1;
+for(let i=1; i<=5; i++){
+    multiply = multiply*i
+}
+//console.log(multiply);
+
+let multiply2=1;
+let i=1;
+while(i<=5){
+    multiply2 = multiply2 * i;
+    i++
+}
+console.log(multiply2);
+
+// For example; 6 ==> 1, 2, 3, 6 ==> Print on the console 4
+// 12 ==> 1, 2, 3, 4, 6, 12 ==> Print on the console 6
+// 5 ==> 1, 5 ==> Print on the console 2
+
+//It should then find sum of the digits of that number. 1175 = 1+1+7+5= 14 
+
