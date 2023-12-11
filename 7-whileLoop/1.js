a40ba85489e8599ae2e9cfa6ebb5d2499731b836
MuratFatih_FEdev_ _ <you@example.com>
2023-12-11 08:06:48 +0100

    added while loop

--- 7-whileLoop/1.js
+++ 7-whileLoop/1.js
@@ -9,14 +9,14 @@ while (a >= b) {
 //5! 5*4*3*2*1
 // for loop ile
 let multiply = 1;
-for(let i=1; i<=5; i++){
-    multiply = multiply*i
+for (let i = 1; i <= 5; i++) {
+    multiply = multiply * i
 }
 //console.log(multiply);
 
-let multiply2=1;
-let i=1;
-while(i<=5){
+let multiply2 = 1;
+let i = 1;
+while (i <= 5) {
     multiply2 = multiply2 * i;
     i++
 }
@@ -28,3 +28,21 @@ console.log(multiply2);
 
 //It should then find sum of the digits of that number. 1175 = 1+1+7+5= 14 
 
+let number = 0;
+let count = 0;
+if (number > 0) {
+    if (number == 1) {
+        console.log(number + " is prime number");
+    } else {
+        for (let i = 2; i < number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            console.log(number + " is prime");
+        } else {
+            console.log(number + " is not prime");
+        }
+    }
+}
