75699ddc271e11b4d7e4e090a09c47310a915d18
MuratFatih_FEdev_ _ <fatihyanik07@gmail.com>
2023-12-21 18:17:13 +0100

    added solution

--- 8-doWhileLoop/5.js
+++ 8-doWhileLoop/5.js
@@ -40,3 +40,16 @@ console.log(food);
 console.log(info);
 
 // parse ve stringfy a bak
+
+
+let name = 'Lydia';
+
+function getName() {
+   // console.log(name);
+    let name = 'Sarah';
+}
+
+getName();
+
+console.log(`\`` === "`")
+console.log(`${1}` === "${1}")
