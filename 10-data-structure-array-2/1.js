a8bee6c8a4e3ae36e755d360a5bea8e3ecb06b80
MuratFatih_FEdev_ _ <fatihyanik07@gmail.com>
2023-12-25 12:13:38 +0100

    added data-structure-array

---
+++ 10-data-structure-array-2/1.js
@@ -0,0 +1,55 @@
+//Declare a variable named "euroCities" and assign an array to the variable
+const euroCities = ["Paris", "London", "Valletta", "Prague", "Rome"];
+//// 2. Change the first item in the array to "Berlin".
+euroCities.splice(0,1,"Berlin") // 0=>index 1=>Kac eleman silinecekse "Berlin" => eklenmek istenen
+console.log(euroCities); //[ 'Berlin', 'London', 'Valletta', 'Prague', 'Rome' ]
+//Print the length of the array "euroCities".
+console.log(euroCities.length);//5
+//Delete the last item of the array "euroCities"
+euroCities.pop()
+console.log(euroCities);//[ 'Berlin', 'London', 'Valletta', 'Prague' ]
+
+//Add "Budapest" to the euroCities array.
+euroCities.push("Budapest")
+console.log(euroCities);//[ 'Berlin', 'London', 'Valletta', 'Prague', 'Budapest' ]
+
+//Delete the second and third items from the euroCities array
+euroCities.splice(1,2);
+console.log(euroCities);//[ 'Berlin', 'Prague', 'Budapest' ]
+
+//Clone the array asianCities.
+const asianCities2 = [...euroCities]
+console.log(asianCities2); // [ 'Berlin', 'Prague', 'Budapest' ]
+
+//Use an array method to select items 2-4 from the array of asianCities and store this in another variable
+const asianCities = ["Tokyo", "Ulaanbaatar", "Beijing", "Seoul", "Hanoi", "Manila"];
+
+const threeAsianCities = asianCities.slice(1,4);
+console.log(threeAsianCities); //[ 'Ulaanbaatar', 'Beijing', 'Seoul']
+
+//Bonus: Use a method to concat euroCities with asianCities. Store the result in a variable (eg. worldCities).
+
+//1.method
+const worldCities = euroCities.concat(asianCities);
+console.log(worldCities);
+
+//2.method
+const worldCities1 = [...euroCities, ...asianCities]
+console.log(worldCities1);['Berlin','Prague','Budapest',  'Tokyo','Ulaanbaatar', 'Beijing','Seoul', 'Hanoi','Manila']
+
+//Reverse the order of worldCities.
+worldCities.reverse();
+console.log(worldCities);
+
+//Replace the 3rd item in the array of worldCities with "Toronto".
+worldCities.splice(2,1, "Toronto");
+console.log(worldCities);
+
+worldCities.splice(1,0,"Washington")
+console.log(worldCities);
+
+const worldCitiesString = worldCities.join(', ')
+console.log(worldCitiesString); //Manila, Washington, Hanoi, Toronto, Beijing, Ulaanbaatar, Tokyo, Budapest, Prague, Berlin
+
+
+
---
+++ 10-data-structure-array-2/Readme.md
@@ -0,0 +1,50 @@
+# 20-PB-datastructure-array-II
+
+# Array Exercises 
+## These exercises are aimed at practicing arrays and array methods. Print each task to the console.
+
+1. Declare a variable named "euroCities" and assign an array to the variable e.g. ["Paris", "London", "Valletta", "Prague", "Rome"]. Declare another variable and assign the second item of the array as a value.
+
+2. Change the first item in the array to "Berlin".
+
+3. Print the length of the array "euroCities".
+
+4. Delete the last item of the array "euroCities".
+
+5. Add "Budapest" to the euroCities array. 
+
+6. Delete the second and third items from the euroCities array. 
+
+7. Create another variable named asianCities and assign an array to the variable. Clone the array asianCities.
+
+8. **Bonus**: Use an array method to select items 2-4 from the array of asianCities and store this in another variable.
+
+9. **Bonus**: Use a method to concat euroCities with asianCities. Store the result in a variable (eg. worldCities).
+
+10. Reverse the order of worldCities.
+
+11. **Bonus**: Replace the 3rd item in the array of worldCities with "Toronto".
+
+12. **Bonus**: Remove no elements from the array of worldCities, but insert "Washington" at the 2nd position.
+
+13. **Bonus** Write a program to join all elements of the result (worldCities) into a string.
+    Example: worldCities = ["Berlin", "London", "Bangkok", "Phnom Penh"];
+    Expected Outputs:
+    "Berlin , London, Bangkok, Phnom Penh"
+    "Berlin+London+Bangkok+Phnom Penh"
+
+**Bonus**
+
+1. Write a program to reverse the string: "Hello World".
+
+**Extra Practice**
+
+Print the results to the console.
+
+1. Make an array of your siblings' names or your favorite movie characters' names.
+2. Make an array of your parents' names.
+3. Combine these two arrays.
+4. Add your pets' names.
+5. Reverse the order of the array.
+6. Access one of your parents' names.
+7. Update the name of one of your parents.
