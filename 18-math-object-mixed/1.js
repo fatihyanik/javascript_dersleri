a0cd643e438eece66239134f3ef8d128b33ff582
MuratFatih_FEdev_ _ <fatihyanik07@gmail.com>
2024-01-01 21:48:37 +0100

    added math object

--- 18-math-object-mixed/1.js
+++ 18-math-object-mixed/1.js
@@ -12,7 +12,24 @@
 
 let names = 'Saliha Büsra MehmetAli Mehmet Fatih Bahadir Erol Ahmet Harun Huseyin Hilal Yanik Serdar Devrim Fidan Ersan Yusuf Erdal';
 let nameArray = names.split(' ');
+console.log(nameArray);
 let randomName = nameArray[Math.floor(Math.random() * nameArray.length)];
 
 console.log(`Hallo ${randomName}`);
 
+let numbers = '-1 7 -33 -8 9 -13 30 -53 -42 -61';
+console.log(numbers);
+let numbersArray = numbers.split(" ");
+console.log(numbers, numbersArray);
+let randomNumbers = Math.floor(Math.random() * numbersArray.length);
+let randomNumberFromTheNumString = Math.abs(numbersArray[randomNumbers])
+console.log(randomNumberFromTheNumString);
+
+//cozüm 2 (by MEHMET KARACA)
+let numbers2 = '-1 7 -33 -8 9 -13 30 -53 -42 -61';
+console.log(numbers2);
+let numbers2Array = numbers2.split(" ");
+console.log(numbers2, numbers2Array);
+let randomNumbers2 = Math.floor(Math.random() * numbers2Array.length);
+let randomNumberFromString = numbers2Array[randomNumbers2] <= 0 ? numbers2Array[randomNumbers2] * (-1) : numbers2Array[randomNumbers2] * 1
+console.log(randomNumberFromString);
