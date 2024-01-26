fdf8f93666fb5dac08d7f485e2611ce3a3b83116
fatihyanik <fatihyanik07@gmail.com>
2024-01-26 21:31:55 +0100

    added promises

---
+++ 26-promises/1.js
@@ -0,0 +1,82 @@
+//Callback
+// const doSomething = callback => {
+//     setTimeout(() => {
+//       const skills = ['HTML', 'CSS', 'JS']
+//       callback('It did not go well', skills)
+//     }, 2000)
+//   }
+
+//   const callback = (err, result) => {
+//     if (err) {
+//       return console.log(err)
+//     }
+//     return console.log(result)
+//   }
+
+//   doSomething(callback)
+
+
+// syntax
+// syntax
+// const promise = new Promise((resolve, reject) => {
+//     resolve('success')
+//     reject('failure')
+//   })
+
+
+// const promise1 = new Promise((resolve, reject) => {
+//    setTimeout(()=>{
+//     resolve('Promise resolved!')
+//    },1500)
+//   })
+
+//   promise1.then(result => {
+//     console.log(result)
+//   }).catch(err => {
+//     console.log(err);
+//   })
+
+// Promise
+// const doPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const skills = ['HTML', 'CSS', 'JS']
+//         if (skills.length > 0) {
+//             resolve(skills)
+//         } else {
+//             reject('Something wrong has happened')
+//         }
+//     }, 2000)
+// })
+
+// doPromise
+//     .then(result => {
+//         console.log(result) //[ 'HTML', 'CSS', 'JS' ]
+//     })
+//     .catch(error => console.log(error))
+
+// Promise
+const doPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const skills = ['HTML', 'CSS', 'JS']
+        if (skills.includes('Node')) {
+            resolve('fullstack developer')
+        } else {
+            reject('Something wrong has happened')
+        }
+    }, 2000)
+})
+
+doPromise
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => console.error(error)) //Something wrong has happened
+
+    const url = 'https://restcountries.com/v2/all' // countries api
+    fetch(url)
+      .then(response => response.json()) // API verilerine JSON olarak erişme
+      .then(data => {
+        // verileri almak
+        console.log(data)
+      })
+      .catch(error => console.error(error)) // yanlış bir şey olursa işleme hatası
