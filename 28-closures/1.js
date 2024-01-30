74fded5a95f13bd4b6051b394e7f9bc941983753
fatihyanik <fatihyanik07@gmail.com>
2024-01-30 21:05:02 +0100

    added closures

---
+++ 28-closures/1.js
@@ -0,0 +1,35 @@
+function outerFunction() {
+    let count = 0;
+    function innerFunction() {
+        count++
+        return count
+    }
+
+    return innerFunction
+}
+const innerFunc = outerFunction()
+
+console.log(innerFunc())
+console.log(innerFunc())
+console.log(innerFunc())
+
+function outerFunction1() {
+    let count = 0;
+    function plusOne() {
+        count++
+        return count
+    }
+    function minusOne() {
+        count--
+        return count
+    }
+
+    return {
+        plusOne:plusOne(),
+        minusOne:minusOne()
+    }
+}
+const innerFuncs1 = outerFunction1()
+
+console.log(innerFuncs1.plusOne)
+console.log(innerFuncs1.minusOne)
